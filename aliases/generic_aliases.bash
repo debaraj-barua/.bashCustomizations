97f03f2ee18943f4bf276944f41fa1462dd261b4
Debaraj Barua <debaraj.barua@autodesk.com>
2018-09-26 15:43:01 +0200

    [fix][aliases] fix cd aliases

--- aliases/generic_aliases.bash
+++ aliases/generic_aliases.bash
@@ -3,9 +3,8 @@ alias cd..='cd ..'
  
 ## a quick way to get out of current directory ##
 alias ..='cd ..'
-alias ...='cd ../../../'
-alias ....='cd ../../../../'
-alias .....='cd ../../../../'
+alias ...='cd ../..'
+alias ....='cd ../../../'
 alias .4='cd ../../../../'
 alias .5='cd ../../../../..'
 
